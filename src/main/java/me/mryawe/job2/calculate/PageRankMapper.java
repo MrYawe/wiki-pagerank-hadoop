2806e87bf2860c3ff9caa1f5bc916f83809cb1c2
Jihad Mustafa <Jihad@Jihads-MacBook-Pro.local>
2017-05-17 17:08:04 +0200

    resolve separator problem

--- src/main/java/me/mryawe/job2/calculate/PageRankMapper.java
+++ src/main/java/me/mryawe/job2/calculate/PageRankMapper.java
@@ -18,10 +18,10 @@ public class PageRankMapper extends Mapper<Text, Text, Text, Text> {
 
 	@Override
 	public void map(Text key, Text value, Context context) throws CharacterCodingException, IOException, InterruptedException {
-		int position = value.find(",");
+		int position = value.find("\t");
 		if (position != -1) {
 		    double oldPageRank = Double.parseDouble(Text.decode(value.getBytes(), 0, position));
-		    String[] links = Text.decode(value.getBytes(), position + 1, value.getLength() - (position + 1)).split("\t");
+		    String[] links = Text.decode(value.getBytes(), position + 1, value.getLength() - (position + 1)).split(",");
 		    for (String link : links) {
 			context.write(new Text(link), new Text("#".concat(String.valueOf(oldPageRank / links.length))));
 		    }
